f03b19640574cbbd8ed558452b47110e347b5314
vlad <vlad2012chesnokov@mail.ru>
2022-03-25 10:57:05 +0500

    main logic

---
+++ src/Card.java
@@ -0,0 +1,57 @@
+import java.util.Arrays;
+
+public class Card {
+    private int rank;
+    private String suit;
+    public static int maxRank;
+
+    Card(int rank, String suit)
+    {
+        this.rank = rank;
+        String [] data = {"diamonds","clubs", "hearts", "spades"};
+        if (Arrays.asList(data).contains(suit)==false)
+            System.out.println("Wrong suit value");
+        if (this.rank>maxRank)
+            maxRank = this.rank;
+    }
+    Card(String suit)
+    {
+        this.suit = suit;
+        this.rank = maxRank+1;
+    }
+    public int getRank() {
+        return rank;
+    }
+    public String getSuit() {
+        return suit;
+    }
+
+    public String ToString() {
+        String [] data = {"Jack", "Queen", "King", "Ace", "Joker"};
+        String rank = Integer.toString(getRank());
+        switch (rank) {
+            case "10":
+                rank = data[0];
+                break;
+            case "11":
+                rank = data[1];
+                break;
+            case "12":
+                rank = data[2];
+                break;
+            case "13":
+                rank = data[3];
+                break;
+            case "14":
+                rank = data[4];
+                break;
+        }
+        return rank;
+    }
+    public static void main(String[] args) {
+        Card card = new Card (10,"diamonds");
+        Card card1 = new Card("clubs");
+        System.out.println(card.getRank());
+        System.out.println(card1.getRank());
+    }
+}
