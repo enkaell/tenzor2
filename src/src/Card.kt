33e41825d9110a484593d390586efcf1e2a2c7f8
vlad <vlad2012chesnokov@mail.ru>
2022-03-31 23:58:33 +0500

    kotlin

--- src/src/Card.kt
+++ src/src/Card.kt
@@ -1,19 +1,29 @@
 package src
 
-import java.util.Arrays
-import java.util.Objects
-
 class Card {
-    var rank = 0
-        private set
+    var rank: Int = 0
     var suit: String
         private set
 
+
+    enum class RankName(val rank: Int){
+        jack(11),
+        queen(12),
+        king(13),
+        ace(14),
+    }
+    enum class SuitName(val suit: String){
+        clubs("clubs"),
+        diamonds("diamonds"),
+        hearts("hearts"),
+        spades("spades");
+    }
+
     internal constructor(rank: Int, suit: String) {
         this.suit = suit
-        val data = arrayOf("diamonds", "clubs", "hearts", "spades")
-        if (Arrays.asList(data).contains(suit) === false) System.out.println("Wrong suit value")
-        if (this.rank > maxRank) maxRank = this.rank else this.rank = rank
+        this.rank = rank
+        if (SuitName.values().equals(this.suit) == false) println("Wrong suit value")
+        if (rank > maxRank) maxRank = rank
     }
 
     internal constructor(suit: String) {
@@ -21,28 +31,17 @@ class Card {
         rank = maxRank + 1
     }
 
-    @Override
     override fun toString(): String {
-        val data = arrayOf("Jack", "Queen", "King", "Ace", "Joker")
-        var rank: String = Integer.toString(rank)
-        when (rank) {
-            "10" -> rank = data[0]
-            "11" -> rank = data[1]
-            "12" -> rank = data[2]
-            "13" -> rank = data[3]
-            "14" -> rank = data[4]
+        var rang: String = Integer.toString(this.rank)
+        when (rang) {
+            "11" -> rang = RankName.jack.name
+            "12" -> rang = RankName.queen.name
+            "13" -> rang = RankName.king.name
+            "14" -> rang = RankName.ace.name
         }
-        return rank
+        return rang
     }
 
-    fun equals(card: Card): Boolean {
-        return rank == card.rank && suit === card.suit
-    }
-
-    @Override
-    override fun hashCode(): Int {
-        return Objects.hash(rank, suit)
-    }
 
     val isStandart: Boolean
         get() = rank >= 6 && rank <= 14
@@ -51,16 +50,28 @@ class Card {
         return Integer.compare(rank, card.rank)
     }
 
-    fun cardEquals(card: Card): Int {
-        val data = arrayOf("clubs", "diamonds", "spades", "hearts")
-        return Arrays.asList(data).indexOf(suit) - Arrays.asList(data).indexOf(card.suit)
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Card
+
+        if (rank != other.rank) return false
+        if (suit != other.suit) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = rank
+        result = 31 * result + suit.hashCode()
+        return result
     }
 
     companion object {
         var maxRank = 0
         fun cardEquals(card1: Card, card2: Card): Int {
-            val data = arrayOf("clubs", "diamonds", "spades", "hearts")
-            return Arrays.asList(data).indexOf(card1.suit) - Arrays.asList(data).indexOf(card2.suit)
+            return SuitName.valueOf(card1.suit).ordinal - SuitName.valueOf(card2.suit).ordinal
         }
     }
 }
--- src/src/main.kt
+++ src/src/main.kt
@@ -1,37 +1,10 @@
 package src
-
-import java.util.*
-
-class main {
-    companion object constructor(args: Array<String?>?)
-
-    init {
-        val card = Card(10, "diamonds")
+    fun main(){
+        val card = Card(11, "diamonds")
         val card1 = Card("clubs")
-        System.out.println(card.getRank())
-        System.out.println(card1.getRank())
-        System.out.println(card.equals(card))
-        System.out.println(card.hashCode())
-        System.out.println(card.cardEquals(card1))
-        val deck: Deck = Deck.CreateDeck(true)
-        val pulledCards: ArrayList<Card> = ArrayList()
-        for (i in 0..17) {
-            pulledCards.add(deck.takeCard())
-        }
-        val indexToDeck: Int = Random().nextInt(pulledCards.size())
-        val cardToDeck: Card = pulledCards.get(indexToDeck)
-        pulledCards.remove(indexToDeck)
-        deck.returnToDeck(cardToDeck)
-        System.out.println(deck.toString())
-        deck.shakeDeck()
-        System.out.println(deck.toString())
-        val root = Folder("root")
-        val gameWoW = File("WoW", "exe", root)
-        val games = Folder("games", root, gameWoW)
-        val gameCS = File("CS", "exe", games)
-        System.out.println(games.returnChilds())
-        System.out.println(gameWoW.path)
-        System.out.println(gameWoW.returnParent())
-        System.out.println(gameCS.path)
+        val card2 = Card(10, "diamonds")
+        println(card.equals(card2))
+        println(card.hashCode())
+        println(Card.cardEquals(card1, card2))
+        println(card.toString())
     }
-}
